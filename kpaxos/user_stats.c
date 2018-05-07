774007be72cbd868e472db3bf862fd823ece9abf
Emanuele <esposem@usi.ch>
2018-05-07 16:23:54 +0200

    improved client

--- kpaxos/user_stats.c
+++ kpaxos/user_stats.c
@@ -27,7 +27,7 @@ check_timeout(struct client* client)
   for (int i = 0; i < client->nclients; ++i) {
     long diff = timeval_diff(&client->nclients_time[i], &now);
     if (diff > TIMEOUT_US) {
-      printf("Client %d sent expired %ld\n", i + client->id, diff);
+      // printf("Client %d sent expired %ld\n", i + client->id, diff);
       client_submit_value(client, i + client->id);
     }
   }
@@ -49,13 +49,14 @@ update_stats(struct stats* stats, struct timeval delivered, size_t size)
 {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  long lat = timeval_diff(&delivered, &tv);
+  // long lat = timeval_diff(&delivered, &tv);
   stats->delivered_count++;
   stats->delivered_bytes += size;
-  stats->avg_latency =
-    stats->avg_latency + ((lat - stats->avg_latency) / stats->delivered_count);
-  if (stats->min_latency == 0 || lat < stats->min_latency)
-    stats->min_latency = lat;
-  if (lat > stats->max_latency)
-    stats->max_latency = lat;
+  // stats->avg_latency =
+  //   stats->avg_latency + ((lat - stats->avg_latency) /
+  //   stats->delivered_count);
+  // if (stats->min_latency == 0 || lat < stats->min_latency)
+  //   stats->min_latency = lat;
+  // if (lat > stats->max_latency)
+  //   stats->max_latency = lat;
 }
